e102682feee23b8a48fb6c46b255ba956dbde2ab
RubenUrdaneta <urdaneta816@gmail.com>
2023-04-10 08:21:43 -0400

    Comentando Cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { PORT } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const config = new DocumentBuilder()
     .setTitle('InfoTPM API')
@@ -14,13 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
   await app.listen(PORT);
 }
 bootstrap();
