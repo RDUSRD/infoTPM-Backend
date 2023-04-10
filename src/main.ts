2ed7a310fde86b3063b98991872e2edbe90edd5d
RubenUrdaneta <urdaneta816@gmail.com>
2023-04-09 22:09:00 -0400

    Comentando Cors

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,12 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(PORT);
 }
 bootstrap();
